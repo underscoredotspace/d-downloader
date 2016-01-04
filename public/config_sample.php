e58ddf8d02d4be0b4562e1c76fecda24605d38c5
Ampersand Underscore Space <ampersand@underscore.space>
2016-01-04 12:29:47 +0000

    Some clean-up and explanation in config.php. crontab sample provided also.

---
+++ public/config_sample.php
@@ -0,0 +1,4 @@
+<?php
+  $DilbertDomain = "http://YOURSITEADDRESS"; // Must point to our <public> folder
+  $ImgLocation = "/[images location]/"; // Must point to our <store> folder and *NEEDS* trailing slash (sorry)
+?>
