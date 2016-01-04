04665b0e808b77781430f088eb4e6656bacf1b77
Ampersand Underscore Space <ampersand@underscore.space>
2016-01-04 12:58:35 +0000

    explain that trailing slash needed on website address

--- public/config_sample.php
+++ public/config_sample.php
@@ -1,4 +1,4 @@
 <?php
-  $DilbertDomain = "http://YOURSITEADDRESS"; // Must point to our <public> folder
+  $DilbertDomain = "http://YOURSITEADDRESS/"; // Must point to our <public> folder *NEEDS* trailing slash (oops)
   $ImgLocation = "/[images location]/"; // Must point to our <store> folder and *NEEDS* trailing slash (sorry)
 ?>
